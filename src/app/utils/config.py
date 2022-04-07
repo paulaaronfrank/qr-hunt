5ece2b0f9ccbeab926f7dc18010f129b3535cfc7
Aaron Frank <aaron@Aarons-MacBook-Pro.local>
2022-04-07 12:31:22 +0200

    update url

--- src/app/utils/config.py
+++ src/app/utils/config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     ADMIN_USER = os.environ.get('ADMIN_USER', 'admin')
     ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', 'admin')
-    BASE_URL = os.environ.get('BASE_URL', 'https://54.194.204.59/')
+    BASE_URL = os.environ.get('BASE_URL', 'https://smartconference.network/')
     COLOR = os.environ.get('COLOR', '#000000')
     IMG_URL = os.environ.get('IMG_URL', None)
     SECRET = os.environ.get('SECRET', 'secret')
