3dfd80d5a4f42356074ee367d936db48b298e52a
Aaron Frank <aaron@Aarons-MacBook-Pro.local>
2022-04-12 10:13:53 +0200

    admin funcs

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,10 +1,11 @@
-from flask import Flask, send_from_directory, request, render_template, make_response, redirect, url_for, send_file
+from flask import Flask, send_from_directory, request, render_template, make_response, redirect, url_for, send_file, jsonify
 from .utils import Config, create_qr_code
 from uuid import uuid4
 from io import BytesIO
 import os
 import jwt
 import pickle
+import shutil
 
 
 def create_app():
@@ -23,17 +24,23 @@ def create_app():
                                Config.SECRET, algorithm='HS256')
 
             data = pickle.dumps({'name': username, 'codes': set()})
-            with open(f'app/users/{user_id}', 'wb') as fp:
+            with open(f'app/users/{user_id}.usr', 'wb') as fp:
                 fp.write(data)
 
             response = make_response(render_template('score.html', name=username, score=0))
-            response.set_cookie('token', token)
+            response.set_cookie('user', token)
             return response
 
-        if token := request.cookies.get('token'):
+        if token := request.cookies.get('user'):
             decoded = jwt.decode(token, Config.SECRET, algorithms=["HS256"])
-            with open('app/users/' + decoded['user_id'], 'rb') as fp:
-                data = pickle.loads(fp.read())
+            try:
+                with open('app/users/' + decoded['user_id'] + '.usr', 'rb') as fp:
+                    data = pickle.loads(fp.read())
+            except:
+                data = pickle.dumps({'name': decoded['name'], 'codes': set()})
+                with open(f'app/users/{decoded["user_id"]}.usr', 'wb') as fp:
+                    fp.write(data)
+                data = {'name': decoded['name'], 'codes': set()}
             score = len(data['codes'])
             return render_template('score.html', name=decoded['name'], score=score)
         return render_template('register.html')
@@ -41,31 +48,79 @@ def create_app():
     @app.route('/logout')
     def logout():
         response = make_response(redirect(url_for('index')))
-        response.delete_cookie('token')
+        response.delete_cookie('user')
         return response
 
     @app.route('/admin')
     def admin():
-        return create_qr()
+        if request.cookies.get('token'):
+            pass
+        # ToDo: Login Page if no token // Page QR codes (Add) // User Scores (+ Reset) // Options (Password to register + Reset)
+        return render_template('admin_login.html')
+
+    @app.route('/admin/user', methods=['GET', 'DELETE'])
+    def admin_user():
+        if request.method == 'DELETE':
+            for f in os.listdir('app/users'):
+                if not f.endswith(".usr"):
+                    continue
+                os.remove(os.path.join('app/users', f))
+            return 'All Users Reset'
+        scores = []
+        for user in os.listdir('app/users'):
+            if user.endswith(".usr"):
+                with open('app/users/' + user, 'rb') as fp:
+                    data = pickle.loads(fp.read())
+                    scores.append((data['name'], len(data['codes'])))
+        sorted(scores, key=lambda l: l[1])
+        return jsonify(scores)
+
+    @app.route('/admin/qr', methods=['GET', 'POST', 'DELETE'])
+    def admin_qr():
+        if request.method == 'POST':
+            num = min(int(request.args.get('num', 1)), 20)
+            if num > 1:
+                qrs = []
+                for _ in range(num):
+                    qrs.append(create_qr())
+                return 'created'
+            img = create_qr()
+            return send_file(img, mimetype='image/png')
+        if request.method == 'DELETE':
+            for f in os.listdir('app/codes'):
+                if not f.endswith(".png"):
+                    continue
+                os.remove(os.path.join('app/codes', f))
+            for f in os.listdir('app/exports'):
+                if not f.endswith(".zip"):
+                    continue
+                os.remove(os.path.join('app/exports', f))
+            return 'All Deleted.'
+        fn = str(uuid4())
+        shutil.make_archive(f'app/exports/{fn}', 'zip', 'app/codes/')
+        return send_from_directory('exports', f'{fn}.zip')
 
     @app.route('/<qr_id>')
     def check_id(qr_id):
-        if token := request.cookies.get('token'):
+        if token := request.cookies.get('user'):
             qr_codes = [i.split('.')[0] for i in os.listdir(f'{os.getcwd()}/app/codes')]
             if qr_id in qr_codes:
                 decoded = jwt.decode(token, Config.SECRET, algorithms=["HS256"])
 
-                with open('app/users/' + decoded['user_id'], 'rb') as fp:
-                    data = pickle.loads(fp.read())
+                try:
+                    with open('app/users/' + decoded['user_id'] + '.usr', 'rb') as fp:
+                        data = pickle.loads(fp.read())
+                except:
+                    data = {'name': decoded['name'], 'codes': set()}
                 data['codes'].add(qr_id)
                 score = len(data['codes'])
                 data = pickle.dumps(data)
-                with open('app/users/' + decoded['user_id'], 'wb') as fp:
+                with open('app/users/' + decoded['user_id'] + '.usr', 'wb') as fp:
                     fp.write(data)
 
                 return render_template('score.html', name=decoded['name'], score=score)
             return 'Error'
-        return 'Please Register First!'
+        return redirect('index')
 
     return app
 
@@ -80,8 +135,4 @@ def create_qr():
     img_io = BytesIO()
     qr_img.save(img_io, 'PNG')
     img_io.seek(0)
-    return send_file(img_io, mimetype='image/png')
-
-
-def check_qr(id):
-    return
+    return img_io
