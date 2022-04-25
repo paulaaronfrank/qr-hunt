385ed66b468d960a06e68864827a7b62d55a196c
Aaron Frank <aaron@Aarons-MacBook-Pro.local>
2022-04-25 10:20:43 +0200

    qr hlag

--- src/app/utils/config.py
+++ src/app/utils/config.py
@@ -5,7 +5,7 @@ class Config:
     ADMIN_USER = os.environ.get('ADMIN_USER', 'admin')
     ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', 'admin')
     BASE_URL = os.environ.get('BASE_URL', 'https://smartconference.network/')
-    COLOR = os.environ.get('COLOR', '#000000')
+    COLOR = os.environ.get('COLOR', '#121565')
     IMG_URL = os.environ.get('IMG_URL', None)
     SECRET = os.environ.get('SECRET', 'secret')
 
